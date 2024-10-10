5253eac57ba4242abf0ed7842fac2046a6efb535
Ham Petrosyan <hampetrosyan007@gmail.com>
2024-10-10 19:19:21 +0400

    updated Draw component

--- src/Components/Draw/index.jsx
+++ src/Components/Draw/index.jsx
@@ -4,10 +4,6 @@ import styles from "./Draw.module.scss";
 
 export const Draw = ({ gameState }) => {
   switch (gameState) {
-    case GameState.inProgress:
-      return <></>;
-    case GameState.won:
-      return <></>;
     case GameState.draw:
       return <div className={styles.draw}>Draw</div>;
     default:
